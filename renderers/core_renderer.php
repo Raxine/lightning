2db172e8a36f3f61e4b0edf52d5686916e2de655
raxine <simon@camilotti.fr>
2015-12-31 17:51:47 +0100

    cleaning renderer

--- renderers/core_renderer.php
+++ renderers/core_renderer.php
@@ -2,22 +2,6 @@
 
 class core_renderer {
     
-    function menu() {
-        global $CFG;
-        $output = '';
-        $output .= '<ul>';
-        $output .= '<li>';
-        $output .= '<a href="' . $CFG->wwwroot . '/users/view?userid=3' . '">Users</a>';
-        $output .= '</li>';
-        $output .= '<li>';
-        $output .= '<a href="' . $CFG->wwwroot . '/messages/view?id=2&cat=4' . '">Messages</a>';
-        $output .= '</li>';
-        $output .= '<li>';
-        $output .= '<a href="' . $CFG->wwwroot . '/view/32' . '">tt</a>';
-        $output .= '</li>';
-        $output .= '</ul>';
-        
-        return $output;
-    }
+    
     
 }
